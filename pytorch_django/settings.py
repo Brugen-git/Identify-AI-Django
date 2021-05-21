a0df6e6090ad6e37da356d50c602dcfa3ca34739
Brugen-git <74241208+Brugen-git@users.noreply.github.com>
2021-05-21 17:31:57 +0530

    Update settings.py

--- pytorch_django/settings.py
+++ pytorch_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'om+mod^k%vsvrw(^s%7g(0#utf#7m0^qcro211m&0l!im*j@3z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
